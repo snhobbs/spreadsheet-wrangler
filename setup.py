49eb826ea1a998a5802b1e50883cfeb1632c2ec4
simon <simon.hobbs@electrooptical.net>
2022-09-17 03:08:13 -0400

    adding selectable file names

--- setup.py
+++ setup.py
@@ -4,6 +4,11 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as fh:
     LONG_DESCRIPTION = fh.read()
 
+requirements = []
+with open("requirements.txt", "r") as f:
+    for line in f:
+        requirements.append(line.strip())
+
 setup(name='spreadsheet-wrangler',
       version='0.1.1',
       description='Spreadsheet editing tools',
@@ -20,13 +25,7 @@ setup(name='spreadsheet-wrangler',
           "Operating System :: OS Independent",
       ],
       python_requires='>=3.6',
-      install_requires=[
-          "click",
-          "pandas",
-          "numpy",
-          #"json",
-          #"pyxlsx"
-      ],
+      install_requires=requirements,
       test_suite='nose.collector',
       tests_require=['nose'],
       scripts=["spreadsheet_wrangler.py"],
--- spreadsheet_wrangler.py
+++ spreadsheet_wrangler.py
@@ -148,10 +148,10 @@ def uncluster_regex(df: pd.DataFrame, grouped_column: str, expression: str = "[A
 
 
 def uncluster(df: pd.DataFrame, grouped_column: str) -> pd.DataFrame:
-    return uncluster_ast(df, grouped_column)
+    return uncluster_regex(df, grouped_column)
 
-'''ref-des will not be a tuple of all the matching lines, the rest of the line is taken to be the first in the file and carried forward'''
 def cluster(df: pd.DataFrame, on: list, column: str) -> pd.DataFrame:
+    '''ref-des will not be a tuple of all the matching lines, the rest of the line is taken to be the first in the file and carried forward'''
     for pt in on:
         if pt not in df.columns:
             raise KeyError(f"column {pt} or pseudonym not found")
@@ -211,34 +211,48 @@ def gr1():
 @gr1.command("cluster", help='''Cluster spreadsheet by column value''')
 def cluster_command(spreadsheet, on, column, pseudonyms):
     pseudonyms=read_pseodonyms(pseudonyms)
-    fname = os.path.split(os.path.splitext(spreadsheet)[0])[-1]
     df = extract_columns_by_pseudonyms(read_file_to_df(spreadsheet), pseudonyms)
-    cluster(df, on, column).to_excel(f'{fname}_Clustered_On_{column}.xlsx', index=False)
+    if fout is None:
+        base = os.path.split(os.path.splitext(spreadsheet)[0])[-1]
+        fname = f'{base}_Clustered_On_{column}.xlsx'
+    else:
+        fname = fout
+    cluster(df, on, column).to_excel(fname, index=False)
 
+@click.option("--fout", "-o", type=str, default=None, help="Generatated spreadsheet")
 @click.option("--spreadsheet", "-s", type=str, required=True, help="Main spreadsheet")
 @click.option("--column", type=str, required=True, help="Clustered column")
 @click.option("--pseudonyms", "-p", type=str, default="", help="Alternative column names in json format")
 @gr1.command("uncluster", help='''Uncluster spreadsheet by column value''')
-def uncluster_command(spreadsheet, column, pseudonyms):
+def uncluster_command(fout, spreadsheet, column, pseudonyms):
     pseudonyms=read_pseodonyms(pseudonyms)
-    fname = os.path.split(os.path.splitext(spreadsheet)[0])[-1]
     df = extract_columns_by_pseudonyms(read_file_to_df(spreadsheet), pseudonyms)
-    uncluster(df, column).to_excel(f'{fname}_Unclustered_On_{column}.xlsx', index=False)
+    if fout is None:
+        base = os.path.split(os.path.splitext(spreadsheet)[0])[-1]
+        fname = f'{base}_Unclustered_On_{column}.xlsx'
+    else:
+        fname = fout
+    uncluster(df, column).to_excel(fname, index=False)
 
+@click.option("--fout", "-o", type=str, default=None, help="Generatated spreadsheet")
 @click.option("-l", type=str, required=True, help="Left merge")
 @click.option("-r", type=str, required=True, help="Right merge")
 @click.option("--on", type=str, required=True, help="Column to merge on")
 @click.option("--method", default="left", type=click.Choice(["left", "right", "outer", "inner"]), help="Column to merge on")
 @click.option("--pseudonyms", "-p", type=str, default="", help="Alternative column names in json format")
 @gr1.command(help='''Merge two spreadsheets on the given column''')
-def merge(l, r, on, method, pseudonyms):
+def merge(fout, l, r, on, method, pseudonyms):
     pseudonyms=read_pseodonyms(pseudonyms)
     left = extract_columns_by_pseudonyms(read_file_to_df(l), pseudonyms)
     right = extract_columns_by_pseudonyms(read_file_to_df(r), pseudonyms)
     df = left.merge(right, on=on, how=method) # include all DNPs, unknown parts won't cause an error
     fname_l = os.path.split(os.path.splitext(l)[0])[-1]
     fname_r = os.path.split(os.path.splitext(r)[0])[-1]
-    df.to_excel(f'{fname_l}_Merged{fname_r}_On_{on}.xlsx', index=False)
+    if fout is None:
+        fname = f'{fname_l}_Merged{fname_r}_On_{on}.xlsx'
+    else:
+        fname = fout
+    df.to_excel(fname, index=False)
 
 @click.option("-l", type=str, required=True, help="First spreadsheet")
 @click.option("-r", type=str, required=True, help="Second spreadsheet")
@@ -271,11 +285,16 @@ def filter_command(spreadsheet, on, value, column, blank_defaults, pseudonyms):
     pseudonyms=read_pseodonyms(pseudonyms)
     df = extract_columns_by_pseudonyms(read_file_to_df(spreadsheet), pseudonyms)
     filtered_df = filter_df(df, on, value, column, blank_defaults)
-    fname = os.path.split(os.path.splitext(spreadsheet)[0])[-1]
-    filtered_df.to_excel(f'{fname}_Filtered_On_{on}_{value}_by_{column}.xlsx', index=False)
+    if fout is None:
+        base = os.path.split(os.path.splitext(spreadsheet)[0])[-1]
+        fname = f'{fname}_Filtered_On_{on}_{value}_by_{column}.xlsx'
+    else:
+        fname = fout
+    filtered_df.to_excel(fname, index=False)
 
-@click.option("--fin", "-i", type=str, required=True, help="Input sreadsheet")
-@click.option("--fout", "-o", type=str, help="Generatated spreadsheet")
+
+@click.option("--fin", "-i", type=str, required=True, help="Input spreadsheet")
+@click.option("--fout", "-o", type=str, default=None, help="Generatated spreadsheet")
 @click.option("--format", type=click.Choice(supported_file_formats), help="Generatated spreadsheet format")
 @click.option("--pseudonyms", "-p", type=str, default="", help="Alternative column names in json format")
 @gr1.command("translate", help="Compares the given columns, passes if all given columns exist in both files and values are the same")
@@ -283,14 +302,68 @@ def translate_command(fin, fout, format, pseudonyms):
     pseudonyms=read_pseodonyms(pseudonyms)
     df = extract_columns_by_pseudonyms(read_file_to_df(fin), pseudonyms)
 
-    fname = os.path.split(os.path.splitext(fin)[0])[-1]
     if fout is None:
+        base = os.path.split(os.path.splitext(fin)[0])[-1]
         if format is not None:
-            fout = fname + format
+            fout = base + format
         else:
             fout = fin
 
     df.to_excel(fout, index=False)
+
+@click.option("--fin", "-i", type=str, required=True, help="Input spreadsheet")
+@click.option("--fout", "-o", type=str, default=None, help="Generatated spreadsheet")
+@click.option("--column", "-c", type=str, required=True, help="Column to use as primary value")
+@click.option("--delimiter", "-d", type=str, required=True, help="Current delimiter")
+@click.option("--new-delimiter", "-n", type=str, required=True, help="New delimiter")
+@click.option("--pseudonyms", "-p", type=str, default="", help="Alternative column names in json format")
+@gr1.command("delimiter", help="")
+def delimiter_command(fin, fout, column, delimiter, new_delimiter, pseudonyms):
+    pseudonyms=read_pseodonyms(pseudonyms)
+    df = extract_columns_by_pseudonyms(read_file_to_df(fin), pseudonyms)
+    col = []
+    for _, line in df.iterrows():
+        col.append(new_delimiter.join(line[column].split(delimiter)))
+    df[column] = col
+
+    if fout is None:
+        base = os.path.split(os.path.splitext(fin)[0])[-1]
+        fname = f'{base}_Delimiter_replaced_{column}.xlsx'
+    else:
+        fname = fout
+    df.to_excel(fname, index=False)
+
+@click.option("--fin", "-i", type=str, required=True, help="Input spreadsheet")
+@click.option("--fout", "-o", type=str, default=None, help="Generatated spreadsheet")
+@click.option("--column", "-c", type=str, required=True, help="Column to use as primary value")
+@click.option("--delimiter", "-d", type=str, required=True, help="Current delimiter")
+@click.option("--pseudonyms", "-p", type=str, default="", help="Alternative column names in json format")
+@click.option("--regex", "-r", type=str, default="(\(?\+?[0-9]*\)?)?[0-9_\- \(\)]", help="Regex expression to sort by, defaults to taking the first number")
+@gr1.command("sort-column", help="")
+def sort_lines_in_column(fin, fout, column, delimiter, pseudonyms, regex):
+    '''Break a line by delimiter, sort by number if found otherwise by string'''
+    pseudonyms=read_pseodonyms(pseudonyms)
+    df = extract_columns_by_pseudonyms(read_file_to_df(fin), pseudonyms)
+    col = []
+    regex_compiled = re.compile(regex)
+    for _, line in df.iterrows():
+        entries = line[column].split(delimiter)
+        try:
+            try:
+                sorted_list = sorted(entries, key=lambda x: int(regex_compiled.search(x).group()))
+            except ValueError as e:
+                sorted_list = sorted(entries, key=lambda x: regex_compiled.search(x).group())
+        except TypeError as e:
+            sorted_list = sorted(entries)
+        entry = delimiter.join(sorted_list)
+        col.append(entry)
+    df[column] = col
+    if fout is None:
+        base = os.path.split(os.path.splitext(fin)[0])[-1]
+        fname = f'{fname}_Sorted_{column}.xlsx'
+    else:
+        fname = fout
+    df.to_excel(fname, index=False)
 
 def main():
     gr1()
